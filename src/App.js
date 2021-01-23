f43587dc5963492982e149f953b33fee3b29380f
Isha <isha.terdal05@gmail.com>
2021-01-22 17:12:53 -0800

    final portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import SinglePost from "./Components/SinglePost";
+import Post from "./Components/Post";
+import Project from "./Components/Project";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route component={Home} path='/' exact />
+      <Route component={About} path='/About' />
+      <Route component={SinglePost} path='/Post/:slug' />
+      <Route component={Post} path='/Post' />
+      <Route component={Project} path='/Project' />
+    </Switch>
+    
+    </BrowserRouter>
+ 
+  )
+    
 }
 
 export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import image from "../DSC03130.JPG";
+
+export default function home() {
+        return (
+                <main>
+                        <img src={image} alt="SF skyline" className="absolute object-cover w-full h-full"/>
+                        <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha! I'm Isha</h1>
+                        </section>
+                </main>
+        )
+}
